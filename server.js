4a1b76f11cc6f9ba749915faa79bd21ea25a1fbb
Chris <cehaliga@uwm.edu>
2020-12-17 12:15:58 -0600

    init

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = 'mongodb+srv://cehaliga:123@cluster0.d2rqg.mongodb.net/<dbname>?retryWrites=true&w=majority';
+const uri = process.env.MONGODB_URI || 'mongodb+srv://cehaliga:123@cluster0.d2rqg.mongodb.net/<dbname>?retryWrites=true&w=majority';
 mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex: true});
 mongoose.set('useFindAndModify', false);
 const connection = mongoose.connection;
